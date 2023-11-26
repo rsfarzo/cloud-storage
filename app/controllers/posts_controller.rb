09333c468dbdd48181d35aecba37f3c13e0a96af
Robert Sfarzo <114509292+rsfarzo@users.noreply.github.com>
2023-11-25 23:39:01 -0600

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:user_id, :text, :image) #, :images)
+      params.require(:post).permit(:user_id, :text, :image, :images[] )
     end
 end
