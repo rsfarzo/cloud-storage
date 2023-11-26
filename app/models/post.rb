1fb24a019dc5942776a1916c6033d00221a479eb
Robert Sfarzo <114509292+rsfarzo@users.noreply.github.com>
2023-11-26 03:22:41 +0000

    upload multiple

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  text       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
 class Post < ApplicationRecord
+
+    # has_one_attached :image
+    has_many_attached :images
 end
